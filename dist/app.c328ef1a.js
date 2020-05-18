6c46e814556d0b9521379629d376ec582b2c27d8
Julien <julienbroens@gmail.com>
2020-05-18 16:48:17 +0200

    pré push

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -44135,7 +44135,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43471" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39973" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
