05e99151b24e6845635e5a5b4b1e7e21a393ddb1
Julien <julienbroens@gmail.com>
2020-05-19 22:06:53 +0200

    ready to deploy

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -44141,7 +44141,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36097" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45257" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
